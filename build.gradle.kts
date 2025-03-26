b6af951d690e29d5cd93ea7c1275ef0cde2aa448
Fabian Zeindl <fabian.zeindl@gmail.com>
2025-03-26 06:20:07 +0100

    [code] updated symbol-processing-api, serializer-annotations, kotlinpoet

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.devtools.ksp:symbol-processing-api:2.1.10-1.0.30")
-    implementation("com.fab1an:kotlin-json-stream-serializer-annotations:2.0.0")
-    implementation("com.squareup:kotlinpoet:2.0.0")
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.1.20-1.0.31")
+    implementation("com.fab1an:kotlin-json-stream-serializer-annotations:2.0.1")
+    implementation("com.squareup:kotlinpoet:2.1.0")
     implementation("com.fab1an:kotlin-json-stream:1.2.3")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
